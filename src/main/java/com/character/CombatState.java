7fba835cf1a3f18c48894f3d41b3d2fee7e9dd91
Danil Ko <danil.ko@evolphin.com>
2026-03-19 01:00:25 -0700

    Issue-1: Update aim behavior for camera

--- src/main/java/com/character/CombatState.java
+++ src/main/java/com/character/CombatState.java
@@ -20,6 +20,15 @@ public class CombatState extends Resource {
   @RegisterProperty
   public double combatAccelerationFactor = 1.0f;
 
+  @Export
+  @RegisterProperty
+  public double cameraDistance = 3.0f;
+
+
+  @Export
+  @RegisterProperty
+  public double cameraShoulderOffset = 0.1f;
+
 
   // Default constructor is required for Godot to instantiate the Resource
   public CombatState() {
